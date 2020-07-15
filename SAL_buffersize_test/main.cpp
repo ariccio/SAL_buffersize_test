12791b99bc618f9bba3bb75035637e2bc4f6e6b6
Alexander Riccio <test35965@gmail.com>
2020-07-15 16:25:07 -0400

    cleanup

--- SAL_buffersize_test/main.cpp
+++ SAL_buffersize_test/main.cpp
@@ -1,6 +1,14 @@
 #include <windows.h>
 #include <memory>
 
+//#pragma warning(disable: 26481)
+#pragma warning(disable: 26485) //warning C26485: Expression 'fdi->FileName': No array to pointer decay (bounds.3).
+#pragma warning(disable: 26408) //warning C26408: Avoid malloc() and free(), prefer the nothrow version of new with delete (r.10).
+#pragma warning(disable: 26481) //warning C26481: Don't use pointer arithmetic. Use span instead (bounds.1).
+#pragma warning(disable: 26490) //warning C26490: Don't use reinterpret_cast (type.1).
+
+
+
 #pragma pack(push, 1)
 typedef struct /*_Struct_size_bytes_( sizeof(FILE_DIRECTORY_INFORMATION) - sizeof(WCHAR) + FileNameLength )*/ _FILE_DIRECTORY_INFORMATION {
     _Field_range_( 0, ULONG_MAX ) ULONG FileNameLength;
@@ -16,7 +24,7 @@ constexpr rsize_t FDIRequiredBufferBytes( const rsize_t name_length ) {
 	//sizeof(FILE_DIRECTORY_INFORMATION) == 6U, includes two bytes for WCHAR[1]
 
 	const rsize_t name_bytes = FDIFileNameRequiredBufferBytesWithNull( name_length );
-	const rsize_t struct_size_no_filename = sizeof( FILE_DIRECTORY_INFORMATION ) - sizeof(WCHAR);
+	constexpr const rsize_t struct_size_no_filename = sizeof( FILE_DIRECTORY_INFORMATION ) - sizeof(WCHAR);
 	const rsize_t struct_size_with_filename = struct_size_no_filename + name_bytes;
 	return struct_size_with_filename;
 	}
@@ -26,7 +34,7 @@ constexpr rsize_t FDIRequiredBufferBytes( const rsize_t name_length ) {
 _At_(fdi->FileName, _Pre_writable_byte_size_( fdi->FileNameLength ) )
 //_Pre_satisfies_(_String_length_( src ) < ( fdi->FileNameLength ))
 //_Pre_satisfies_( src_length < fdi->FileNameLength )
-void copy_into_buffer( FILE_DIRECTORY_INFORMATION* const fdi, PCWSTR const src, const rsize_t src_length ) {
+void copy_into_buffer( FILE_DIRECTORY_INFORMATION* const fdi, PCWSTR const src, const rsize_t src_length ) noexcept {
 	//deliberately wrong destsz/_SizeInWords
 	wcscpy_s(fdi->FileName, src_length, src);
 	}
@@ -38,12 +46,17 @@ int main() {
 	constexpr rsize_t struct_size_with_filename = FDIRequiredBufferBytes( name_length );
 	void* const void_buffer_pointer = std::malloc( struct_size_with_filename );
 	if( void_buffer_pointer == nullptr ) {
-		return E_OUTOFMEMORY;
+		return ERROR_OUTOFMEMORY;
 		}
 
 
 	FILE_DIRECTORY_INFORMATION* const fdi = static_cast<FILE_DIRECTORY_INFORMATION*>(void_buffer_pointer);
 	fdi->FileNameLength = name_bytes;
+
+	//It complains here, but it doesn't know the correct size of the array?
+	// warning C26483: Value 100 is outside the bounds (0, 0) of variable 'fdi->FileName'. Only index into arrays using constant expressions that are within bounds of the array (bounds.2).
+	fdi->FileName[100] = 0;
+
 
 	const wchar_t str_10_backwards[] = L"987654321";
 
@@ -58,7 +71,7 @@ int main() {
 	
 	//This is explicitly wrong. It should complain. It doesn't.
 	if( sizeof(str_19) > fdi->FileNameLength) {
-		const rsize_t str_19_length = sizeof(str_19)/sizeof(wchar_t);
+		constexpr const rsize_t str_19_length = sizeof(str_19)/sizeof(wchar_t);
 		wcscpy_s(fdi->FileName, str_19_length, str_19 );
 		copy_into_buffer(fdi, str_19, str_19_length);
 		}
@@ -79,8 +92,6 @@ int main() {
 		FILE_DIRECTORY_INFORMATION* const bad_fdi = reinterpret_cast<FILE_DIRECTORY_INFORMATION*>( pointer_to_fdi + 1000 );
 		bad_fdi->FileName[ 0 ] = 0;
 	}
-	//and it should complain here, but it doesn't
-	fdi->FileName[ 100 ] = 0;
 
 	std::free(void_buffer_pointer);
 
