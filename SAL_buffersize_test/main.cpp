67ad65f1e00b2b265d825212687d5df265f99294
Alexander Riccio <test35965@gmail.com>
2020-07-15 16:34:53 -0400

    cleanup

--- SAL_buffersize_test/main.cpp
+++ SAL_buffersize_test/main.cpp
@@ -1,17 +1,16 @@
 #include <windows.h>
 #include <memory>
 
-//#pragma warning(disable: 26481)
-#pragma warning(disable: 26485) //warning C26485: Expression 'fdi->FileName': No array to pointer decay (bounds.3).
 #pragma warning(disable: 26408) //warning C26408: Avoid malloc() and free(), prefer the nothrow version of new with delete (r.10).
+#pragma warning(disable: 26446) //warning C26446: Prefer to use gsl::at() instead of unchecked subscript operator (bounds.4).
 #pragma warning(disable: 26481) //warning C26481: Don't use pointer arithmetic. Use span instead (bounds.1).
+#pragma warning(disable: 26482) //warning C26482: Only index into arrays using constant expressions (bounds.2).
+#pragma warning(disable: 26485) //warning C26485: Expression 'fdi->FileName': No array to pointer decay (bounds.3).
 #pragma warning(disable: 26490) //warning C26490: Don't use reinterpret_cast (type.1).
-
-
 
 #pragma pack(push, 1)
 typedef struct /*_Struct_size_bytes_( sizeof(FILE_DIRECTORY_INFORMATION) - sizeof(WCHAR) + FileNameLength )*/ _FILE_DIRECTORY_INFORMATION {
-    _Field_range_( 0, ULONG_MAX ) ULONG FileNameLength;
+    /*_Field_range_( 0, ULONG_MAX ) */ ULONG FileNameLength;
     _Field_size_bytes_(FileNameLength) WCHAR FileName[1];
 } FILE_DIRECTORY_INFORMATION, *PFILE_DIRECTORY_INFORMATION;
 #pragma pack(pop)
@@ -30,8 +29,9 @@ constexpr rsize_t FDIRequiredBufferBytes( const rsize_t name_length ) {
 	}
 
 
+//Annotations do not seem to help.
 //_At_( fdi->FileNameLength,  )
-_At_(fdi->FileName, _Pre_writable_byte_size_( fdi->FileNameLength ) )
+//_At_(fdi->FileName, _Pre_writable_byte_size_( fdi->FileNameLength ) )
 //_Pre_satisfies_(_String_length_( src ) < ( fdi->FileNameLength ))
 //_Pre_satisfies_( src_length < fdi->FileNameLength )
 void copy_into_buffer( FILE_DIRECTORY_INFORMATION* const fdi, PCWSTR const src, const rsize_t src_length ) noexcept {
@@ -77,6 +77,7 @@ int main() {
 		}
 
 	//Ok, it should complain about this, and it does.
+	//Buffer overrun while writing to 'raw_buffer_pointer':  the writable size is '26' bytes, but '1001' bytes might be written.SAL_buffersize_test	
 	{
 		char* const raw_buffer_pointer = static_cast<char*>( void_buffer_pointer );
 		raw_buffer_pointer[ 1000 ] = 0;
@@ -84,6 +85,7 @@ int main() {
 	//and, it should complain about this...
 	{
 		//here, it does
+		//warning C6386: Buffer overrun while writing to 'pointer_to_fdi':  the writable size is '26' bytes, but '1001' bytes might be written.
 		char* const pointer_to_fdi = reinterpret_cast<char*>( fdi );
 		pointer_to_fdi[ 1000 ] = 0;
 
@@ -91,6 +93,10 @@ int main() {
 		//here it doesn't
 		FILE_DIRECTORY_INFORMATION* const bad_fdi = reinterpret_cast<FILE_DIRECTORY_INFORMATION*>( pointer_to_fdi + 1000 );
 		bad_fdi->FileName[ 0 ] = 0;
+		
+
+		//It should know here that I'm past the length of the valid buffer?
+		fdi->FileName[fdi->FileNameLength + 100] = 0;
 	}
 
 	std::free(void_buffer_pointer);
